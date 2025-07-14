01ca07d0f9c80bfcb4975c1751c54d397933f86b
Pavlo Nesterenko <eseichmusic@gmail.com>
2025-07-14 15:33:51 +0300

    feat: base view

--- FoxmindEd_Learning.WPF/MainWindow.xaml.cs
+++ FoxmindEd_Learning.WPF/MainWindow.xaml.cs
@@ -1,11 +1,20 @@
-﻿using System.Windows;
+﻿using System.Collections.ObjectModel;
+using System.Windows;
 
 namespace FoxmindEd_Learning.WPF;
 
 public partial class MainWindow : Window
 {
+    public ObservableCollection<object> ItemsList { get; set; } = [];
+
     public MainWindow()
     {
+        DataContext = this;
         InitializeComponent();
+    }
+
+    private void SubmitNewEntityObject_Click(object sender, RoutedEventArgs e)
+    {
+
     }
 }
